7a65b221d271ce09c461e91b43cd29ff9fb5261e
Yasin Kaya <yasinkaya.121@gmail.com>
2019-07-14 22:23:07 +0300

    changed installation path of qmi

--- Sixfab_QMI_Installer/qmi_install.sh
+++ Sixfab_QMI_Installer/qmi_install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 : '
 QMI installation script by Sixfab
@@ -6,38 +6,35 @@ Created By Metin Koc, Nov 2018
 Modified by Saeed Johar,Yasin Kaya, June 2019
 '
 
-# Stop on the first sign of trouble
-set -e
-
-if [ $UID != 0 ]; then
-    echo "ERROR: Operation not permitted. Forgot sudo?"
-    exit 1
-fi
-
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
 SET='\033[0m'
 
-echo "${YELLOW}Clear old files${SET}"
-rm -rf ~/files
-rm -rf ~/files.zip
+# clean old installation for new one. 
 
-INS_DIR=~/files
+systemctl stop qmi_reconnect.service
+systemctl disable qmi_reconnect.service
 
-echo "${YELLOW}Change directory to ~ ${SET}"
-cd ~
+if [ -d "/home/pi/files" ]; then rm -rf /home/pi/files ; fi # for old directory 
+if [ -d "$INS_DIR" ]; then rm -rf $INS_DIR; fi
 
-echo "${YELLOW}Downloading source files${SET}"
+INS_DIR=/usr/local/files
+mkdir -p $INS_DIR
+
+echo -e "${YELLOW}Change directory to $INS_DIR ${SET}"
+pushd $INS_DIR
+
+echo -e "${YELLOW}Downloading source files${SET}"
 wget https://github.com/sixfab/Sixfab_RPi_3G-4G-LTE_Base_Shield/raw/master/tutorials/QMI_tutorial/src/quectel-CM.zip
 unzip quectel-CM.zip -d $INS_DIR && rm -r quectel-CM.zip
 
 
-#echo "${YELLOW}Updating rpi${SET}"
+#echo -e "${YELLOW}Updating rpi${SET}"
 #apt update
 
-#echo "${YELLOW}Downlading kernel headers${SET}"
+#echo -e "${YELLOW}Downlading kernel headers${SET}"
 #apt install raspberrypi-kernel-headers
-echo "${YELLOW}Checking Kernel${SET}"
+echo -e "${YELLOW}Checking Kernel${SET}"
 
 case $(uname -r) in
     4.14*) echo $(uname -r) based kernel found
@@ -51,26 +48,24 @@ case $(uname -r) in
     *) echo "Driver for $(uname -r) kernel not found";exit 1;
 esac
 
-echo "${YELLOW}Installing udhcpc${SET}"
+echo -e "${YELLOW}Installing udhcpc${SET}"
 apt install udhcpc
 
-echo "${YELLOW}Copying udhcpc default script${SET}"
+echo -e "${YELLOW}Copying udhcpc default script${SET}"
 mkdir -p /usr/share/udhcpc
 cp -r  $INS_DIR/quectel-CM/default.script /usr/share/udhcpc/
 chmod +x /usr/share/udhcpc/default.script
 
-echo "${YELLOW}Change directory to /home/pi/files/drivers${SET}"
-pushd $INS_DIR/drivers
+echo -e "${YELLOW}Change directory to /home/pi/files/drivers${SET}"
+cd $INS_DIR/drivers
 make && make install
-popd
 
-echo "${YELLOW}Change directory to /home/pi/files/quectel-CM${SET}"
-pushd $INS_DIR/quectel-CM
+echo -e "${YELLOW}Change directory to /home/pi/files/quectel-CM${SET}"
+cd $INS_DIR/quectel-CM
 make
-popd
 
-echo "${YELLOW}After reboot please follow commands mentioned below${SET}"
-echo "${YELLOW}go to $INS_DIR/quectel-CM and run sudo ./quectel-CM -s [YOUR APN]${SET}"
+echo -e "${YELLOW}After reboot please follow commands mentioned below${SET}"
+echo -e "${YELLOW}go to $INS_DIR/quectel-CM and run sudo ./quectel-CM -s [YOUR APN] for manual operation${SET}"
 
 # read -p "Press ENTER key to reboot" ENTER
 # reboot
